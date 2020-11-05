11b34fb55d5d383b94d4ea21572463619ca9ecdd
khaftab <kh7aftab8uddin6ahmed@gmail.com>
2020-11-05 21:08:44 +0530

    update menu items with withRouter HOC componnet

---
+++ src/Pages/homepage/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Directory from '../../Components/directory/Directory'
+import './homepage.styles.scss'
+
+function Homepage() {
+    return (
+        <div className='homepage'>
+            <Directory />
+        </div>
+    )
+}
+
+export default Homepage
