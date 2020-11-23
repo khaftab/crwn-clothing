7939c1e516144c7b5fa2ff0c32af3383e174bc42
khaftab <kh7aftab8uddin6ahmed@gmail.com>
2020-11-23 21:51:16 +0530

    renaming file path of Homepage component

---
+++ src/Pages/homepage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Directory from '../../Components/directory/Directory'
+import './homepage.styles.scss'
+
+function HomePage() {
+    return (
+        <div className='homepage'>
+            <Directory />
+        </div>
+    )
+}
+
+export default HomePage
