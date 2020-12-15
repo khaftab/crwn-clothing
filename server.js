bfca41e4e2e2996ee2515b4fa333ba5ec3c8675e
khaftab <kh7aftab8uddin6ahmed@gmail.com>
2020-12-15 22:04:38 +0530

    fix server.js

--- server.js
+++ server.js
@@ -1,25 +1,81 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const path = require('path')
-const compression = require('compression')
-const enforce = require('express-sslify')
+// const express = require('express')
+// const cors = require('cors')
+// const bodyParser = require('body-parser')
+// const path = require('path')
+// const compression = require('compression')
+// const enforce = require('express-sslify')
 
-if (process.env.NODE_ENV !== 'production') require('dotenv').config()
+// if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+// const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
-const app = express()
-const port = process.env.PORT || 5000
+// const app = express()
+// const port = process.env.PORT || 5000
 
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(cors())
+
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(compression())
+//     app.use(enforce.HTTPS({ trustProtoHeader: true }))
+//     app.use(express.static(path.join(__dirname, 'client/build')));
+
+//     app.get('/service-worker.js', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+//     });
+
+//     app.get('*', function (req, res) {
+//         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+//     });
+
+// }
+
+// app.listen(port, error => {
+//     if (error) throw error;
+//     console.log(`Server is runnig on Port ${port}`)
+// })
+
+
+
+// app.post('/payment', (req, res) => {
+//     const body = {
+//         source: req.body.token.id,
+//         amount: req.body.amount,
+//         currency: 'inr'
+//     }
+
+//     stripe.charges.create(body, (stripeErr, stripeRes) => {
+//         if (stripeErr) {
+//             res.status(500).send({ error: stripeErr })
+//         } else {
+//             res.status(200).send({ success: stripeRes })
+//         }
+//     })
+// })
+
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+var enforce = require('express-sslify');
+
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+const app = express();
+
+
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
+app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(compression())
-    app.use(enforce.HTTPS({ trustProtoHeader: true }))
     app.use(express.static(path.join(__dirname, 'client/build')));
 
     app.get('/service-worker.js', (req, res) => {
@@ -29,28 +85,25 @@ if (process.env.NODE_ENV === 'production') {
     app.get('*', function (req, res) {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
     });
-
 }
 
 app.listen(port, error => {
     if (error) throw error;
-    console.log(`Server is runnig on Port ${port}`)
-})
-
-
+    console.log('Server running on port ' + port);
+});
 
 app.post('/payment', (req, res) => {
     const body = {
         source: req.body.token.id,
         amount: req.body.amount,
-        currency: 'inr'
-    }
+        currency: 'usd'
+    };
 
     stripe.charges.create(body, (stripeErr, stripeRes) => {
         if (stripeErr) {
-            res.status(500).send({ error: stripeErr })
+            res.status(500).send({ error: stripeErr });
         } else {
-            res.status(200).send({ success: stripeRes })
+            res.status(200).send({ success: stripeRes });
         }
-    })
-})
+    });
+});
